3ba9892fc70ec4570fbc5fa073d9a274c27b2318
terjeee <96795863+terjeee@users.noreply.github.com>
2023-02-27 08:00:29 +0100

    +CardPlan

---
+++ src/components/CardPlan.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let title: String;
+</script>
+
+<article class="w-1/2 p-8 border border-black border-opacity-25 rounded">
+  <header class="border-b-2 border-clr-green">
+    <h2>
+      {title}
+    </h2>
+    <h4>$$ /MONTH</h4>
+  </header>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+  </ul>
+</article>
