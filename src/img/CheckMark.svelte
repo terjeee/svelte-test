427ce7a71d2fd1767b080fac2945f3a1b20592b5
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-01 08:29:45 +0100

    mindre checkmark

--- src/img/CheckMark.svelte
+++ src/img/CheckMark.svelte
@@ -1,4 +1,4 @@
-<svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="10" height="8" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path
     d="M1.6875 4.50363L4.89561 7.71039L11.3091 1.29688"
     stroke="#22AD01"
