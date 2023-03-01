cd72594287d459c5cd386c2c69452571a8462248
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-01 08:29:57 +0100

    :)

--- src/components/CardSubscriptionPlan.svelte
+++ src/components/CardSubscriptionPlan.svelte
@@ -4,20 +4,26 @@
   import CheckMark from "../img/CheckMark.svelte";
 
   export let plan: SubscriptionPlan;
+  const { id, title, priceMonth, priceYear, features } = plan;
+  export let checked: boolean;
 </script>
 
 <article class="w-1/2 p-8 border border-black border-opacity-25 rounded">
   <header class="border-b-2 border-clr-green">
-    <h2>{plan.title}</h2>
-    <h4>$ <span>{plan.price}</span> /MONTH</h4>
+    <h2>{title}</h2>
+    <h4>
+      $ {checked ? priceMonth : priceYear} /{checked ? "YEAR" : "MONTH"}
+    </h4>
   </header>
-  <ul>
-    {#each plan.features as feature}
-      <li class="flex">
+  <ul class="py-6">
+    {#each features as feature}
+      <li class="py-2 flex items-center gap-2">
         <CheckMark />
-        {feature}
+        <span class="text-sm">
+          {feature}
+        </span>
       </li>
     {/each}
   </ul>
-  <button id={plan.id}>START HERE</button>
+  <button id={`${id + "_" + checked ? "month" : "year"}`}>START HERE</button>
 </article>
