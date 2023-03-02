9d49cb150334e8bfc64ed55d0e49f74f130af668
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-02 04:26:02 +0100

    fikset ts-type på pageData

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  import type { SubscriptionPlan } from "../types/Subscription";
-  interface Data {
-    subscriptions: SubscriptionPlan[];
-  }
+  import type { PageData } from "./$types";
 
   import MaxWidth from "../components/MaxWidth.svelte";
   import ToggleSwitch from "../components/ToggleSwitch.svelte";
   import CardSubscriptionPlan from "../components/CardSubscriptionPlan.svelte";
   import "../app.css";
 
-  export let data: Data;
+  export let data: PageData;
   const { subscriptions } = data;
 
   let checked: boolean = false;
@@ -17,18 +14,20 @@
 
 <MaxWidth width={"max-w-screen-lg"}>
   <main>
-    <section class="mt-32 flex justify-between">
-      <h2 class="text-2xl">Plans & Pricing</h2>
-      <div class="flex items-center gap-4 text-sm">
+    <section class="mt-32 flex justify-between items-end">
+      <h2 class="text-3xl font-lora tracking-wide">Plans & Pricing</h2>
+      <div class="flex items-center gap-4 text-xs tracking-wider">
         <h3 class={!checked ? "font-bold" : ""}>MONTHLY</h3>
-        <ToggleSwitch on:checked={(event) => (checked = event.detail)} />
+        <ToggleSwitch on:checked={(event) => (checked = event.detail)} />¨
+        {checked}
         <h3 class={checked ? "font-bold" : ""}>YEARLY</h3>
       </div>
     </section>
-    <section class=" mt-10 flex gap-6 justify-between">
+    <section class=" mt-10 flex gap-12 justify-between">
       {#each subscriptions as plan}
         <CardSubscriptionPlan {plan} {checked} />
       {/each}
     </section>
+    <section />
   </main>
 </MaxWidth>
