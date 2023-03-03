87bc11b46f68df2b068205404bd86a61b9be9b81
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-03 05:59:41 +0100

    +svg

---
+++ src/img/SvgArrow.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let rotate: Boolean = false;
+</script>
+
+<svg
+  width="18"
+  height="11"
+  viewBox="0 0 18 11"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  transform={rotate ? "scale(-1 -1)" : ""}
+>
+  <path d="M1.00001 0.99989L8.71814 8.71802L16.4363 0.99989" stroke="#22AD01" stroke-width="2" />
+</svg>
