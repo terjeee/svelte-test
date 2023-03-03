bb777ddb32064d85c2b6e32ac7e80f7115503d18
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-03 06:00:47 +0100

    :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import "../components/MaxWidth.svelte";
-  import MaxWidth from "../components/MaxWidth.svelte";
-  import Logo from "../img/Logo.svelte";
+  import "../components/UX/MaxWidth.svelte";
+  import MaxWidth from "../components/UX/MaxWidth.svelte";
+  import Logo from "../img/SvgLogo.svelte";
 </script>
 
 <nav>
@@ -30,6 +30,40 @@
 
 <slot />
 
-<footer class="bg-blue-50 mt-auto">
-  <MaxWidth width={"max-w-screen-xl"}>26. Februar 2023</MaxWidth>
+<footer class="mt-28 mb-14">
+  <MaxWidth width={"max-w-screen-sm"}>
+    <div class="flex justify-evenly">
+      <div>
+        <h2 class="pb-8 font-bold">BONSAI</h2>
+        <ul class="ml-auto flex flex-col gap-2 text-sm">
+          <li><a href="/" class="hover:text-clr-green">About</a></li>
+          <li><a href="/" class="hover:text-clr-green">Careers</a></li>
+          <li><a href="/" class="hover:text-clr-green">Support</a></li>
+          <li><a href="/" class="hover:text-clr-green">Linkedin</a></li>
+          <li><a href="/" class="hover:text-clr-green">Twitter</a></li>
+          <li><a href="/" class="hover:text-clr-green">Privacy policy</a></li>
+          <li><a href="/" class="hover:text-clr-green">Legal</a></li>
+        </ul>
+      </div>
+      <div>
+        <h1 class="pb-8 font-bold">PRODUCT</h1>
+        <ul class="flex flex-col gap-2 text-sm">
+          <li><a href="/" class="hover:text-clr-green">Proposals</a></li>
+          <li><a href="/" class="hover:text-clr-green">Contracts</a></li>
+          <li><a href="/" class="hover:text-clr-green">Invoicing</a></li>
+          <li><a href="/" class="hover:text-clr-green">Client CRM</a></li>
+          <li><a href="/" class="hover:text-clr-green">Time Tracking</a></li>
+          <li><a href="/" class="hover:text-clr-green">Forms</a></li>
+          <li><a href="/" class="hover:text-clr-green">Accounating</a></li>
+          <li><a href="/" class="hover:text-clr-green">Bonsai Tax</a></li>
+          <li><a href="/" class="hover:text-clr-green">Bonsai Cash</a></li>
+        </ul>
+        <br />
+        <h2 class="text-clr-green font-bold">Pricing</h2>
+        <ul>
+          <li>Bonsai Reviews</li>
+        </ul>
+      </div>
+    </div>
+  </MaxWidth>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import type { PageData } from "./$types";
 
-  import MaxWidth from "../components/MaxWidth.svelte";
-  import ToggleSwitch from "../components/ToggleSwitch.svelte";
-  import CardSubscriptionPlan from "../components/CardSubscriptionPlan.svelte";
+  import MaxWidth from "../components/UX/MaxWidth.svelte";
+  import ToggleSwitch from "../components/UX/ToggleSwitch.svelte";
+  import CardSubscriptionPlan from "../components/PageComponents/CardSubscriptionPlan.svelte";
+  import AddOns from "../components/PageComponents/AddOns.svelte";
+  import StartHere from "../components/PageComponents/StartHere.svelte";
+  import FaqSingle from "../components/PageComponents/FaqSingle.svelte";
   import "../app.css";
 
   export let data: PageData;
@@ -12,21 +15,31 @@
   let checked: boolean = false;
 </script>
 
-<MaxWidth width={"max-w-screen-lg"}>
-  <main>
-    <section class="mt-32 flex justify-between items-end">
-      <h2 class="text-3xl font-lora tracking-wide">Plans & Pricing</h2>
-      <div class="flex items-center gap-4 text-xs tracking-wider">
-        <h3 class={!checked ? "font-bold" : ""}>MONTHLY</h3>
-        <ToggleSwitch on:checked={(event) => (checked = event.detail)} />¨
-        <h3 class={checked ? "font-bold" : ""}>YEARLY</h3>
+<main>
+  <MaxWidth width={"max-w-screen-lg"}>
+    <section>
+      <div class="mt-14 flex justify-between items-end">
+        <h2 class="text-3xl font-lora tracking-wide">Plans & Pricing</h2>
+        <div class="flex items-center gap-4 text-xs tracking-wider">
+          <h3 class={!checked ? "font-bold" : ""}>MONTHLY</h3>
+          <ToggleSwitch on:checked={(event) => (checked = event.detail)} />
+          <h3 class={checked ? "font-bold" : ""}>YEARLY</h3>
+        </div>
+      </div>
+      <div class=" mt-10 flex gap-12 justify-between">
+        {#each subscriptions as plan}
+          <CardSubscriptionPlan {plan} {checked} />
+        {/each}
       </div>
     </section>
-    <section class=" mt-10 flex gap-12 justify-between">
-      {#each subscriptions as plan}
-        <CardSubscriptionPlan {plan} {checked} />
-      {/each}
+    <AddOns />
+    <StartHere />
+    <section class="my-16 ">
+      <h5 class="py-14 text-center text-2xl text-clr-green">Frequently Asked Questions</h5>
+      <FaqSingle faq={{ question: "How does the 14 day free trial work?", answer: ":)" }} />
+      <FaqSingle faq={{ question: "Can I change plans anytime?", answer: ":)" }} />
+      <FaqSingle faq={{ question: "How do I pause my Bonsai subscription?", answer: ":)" }} />
+      <FaqSingle faq={{ question: "What is your refund policy?", answer: ":)" }} />
     </section>
-    <section />
-  </main>
-</MaxWidth>
+  </MaxWidth>
+</main>
