706684f478c708f34823c1dad5bb207f8c4090a3
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-06 20:01:04 +0100

    +test clamp()

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,12 @@ module.exports = {
         montserrat: ["Montserrat"],
         lora: ["Lora"],
       },
+      fontSize: {
+        "dynamic-xs": "clamp(0.875rem, 4vw, 1rem)",
+      },
+      padding: {
+        dynamic: "clamp(2rem, 2rem, 4rem)",
+      },
       boxShadow: {
         card: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
       },
