eb250191a501df8940f3fb209681ae8b0b2d416e
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-06 20:01:38 +0100

    responsiv

--- src/components/PageComponents/FaqSingle.svelte
+++ src/components/PageComponents/FaqSingle.svelte
@@ -4,16 +4,16 @@
   import SvgArrow from "../../img/SvgArrow.svelte";
   import MaxWidth from "../UX/MaxWidth.svelte";
 
-  export let faq: Faq;
+  export let faqInfo: Faq;
 
   let showAnswer = false;
   // const toggleShowAnswer = () => (showAnswer = !showAnswer);
 </script>
 
-<MaxWidth width={"max-w-screen-sm"}>
-  <div class="py-2 border-b-2 border-opacity-20">
-    <button class="w-full flex justify-between items-center" on:click={() => (showAnswer = !showAnswer)}>
-      <h6 class="py-1">{faq.question}</h6>
+<MaxWidth width={"max-w-screen-sm px-0"}>
+  <div class="py-3 border-b-2 border-opacity-20">
+    <button class="w-full flex gap-4 justify-between items-center text-left" on:click={() => (showAnswer = !showAnswer)}>
+      <h6 class="py-1">{faqInfo.question}</h6>
       {#if showAnswer}
         <SvgArrow rotate={true} />
       {:else}
@@ -21,7 +21,7 @@
       {/if}
     </button>
     {#if showAnswer}
-      <p class="py-2 text-sm">{faq.answer}</p>
+      <p class="py-2 text-sm">{faqInfo.answer}</p>
     {/if}
   </div>
 </MaxWidth>
