7465c33b440ad6a98a23e838ddd4910bd51922d5
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-06 20:01:53 +0100

    responsiv

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,15 +18,15 @@
 <main>
   <MaxWidth width={"max-w-screen-lg"}>
     <section>
-      <div class="mt-14 flex justify-between items-end">
-        <h2 class="text-3xl font-lora tracking-wide">Plans & Pricing</h2>
-        <div class="flex items-center gap-4 text-xs tracking-wider">
+      <div class="mt-14 flex justify-between items-center md:items-end">
+        <h2 class="mr-20 text-3xl font-lora tracking-wide">Plans & Pricing</h2>
+        <div class="flex flex-col items-center gap-2 text-xs tracking-wider sm:flex-row sm:gap-4">
           <h3 class={!checked ? "font-bold" : ""}>MONTHLY</h3>
           <ToggleSwitch on:checked={(event) => (checked = event.detail)} />
           <h3 class={checked ? "font-bold" : ""}>YEARLY</h3>
         </div>
       </div>
-      <div class=" mt-10 flex gap-12 justify-between">
+      <div class="mt-12 flex flex-col gap-8 sm:flex-row sm:justify-between sm:gap-6 md:gap-12">
         {#each subscriptions as plan}
           <CardSubscriptionPlan {plan} {checked} />
         {/each}
@@ -34,12 +34,12 @@
     </section>
     <AddOns />
     <StartHere />
-    <section class="my-16 ">
-      <h5 class="py-14 text-center text-2xl text-clr-green">Frequently Asked Questions</h5>
-      <FaqSingle faq={{ question: "How does the 14 day free trial work?", answer: ":)" }} />
-      <FaqSingle faq={{ question: "Can I change plans anytime?", answer: ":)" }} />
-      <FaqSingle faq={{ question: "How do I pause my Bonsai subscription?", answer: ":)" }} />
-      <FaqSingle faq={{ question: "What is your refund policy?", answer: ":)" }} />
+    <section class="my-10 sm:my-16">
+      <h5 class="py-10 text-center text-2xl text-clr-green">Frequently Asked Questions</h5>
+      <FaqSingle faqInfo={{ question: "How does the 14 day free trial work?", answer: ":)" }} />
+      <FaqSingle faqInfo={{ question: "Can I change plans anytime?", answer: ":)" }} />
+      <FaqSingle faqInfo={{ question: "How do I pause my Bonsai subscription?", answer: ":)" }} />
+      <FaqSingle faqInfo={{ question: "What is your refund policy?", answer: ":)" }} />
     </section>
   </MaxWidth>
 </main>
